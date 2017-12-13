1c6b479a162f2981872b747b65d21e4c8504b1db
kozlov_vn <kozlov_vitali@hotmail.com>
2017-12-14 00:20:11 +0300

    switch off system messages for viewers

--- src/tele_bot/bot.py
+++ src/tele_bot/bot.py
@@ -198,12 +198,12 @@ class TelegramBot(telebot.TeleBot):
         self.send_message(ADMIN_ID, msg)
         self.send_message(ADMIN_ID, TO_RULE, reply_markup=GET_KB)
 
-        for i in range(0, self.__model.get_viewers_len()):
-            (s_id, s_name) = self.__model.get_viewer_by_i(i)
-            self.send_message(s_id, msg)
-            self.send_message(s_id, TO_RULE, reply_markup=GET_KB)
+        # for i in range(0, self.__model.get_viewers_len()):
+        #     (s_id, s_name) = self.__model.get_viewer_by_i(i)
+        #     self.send_message(s_id, msg)
+        #     self.send_message(s_id, TO_RULE, reply_markup=GET_KB)
 
-            logger.info(msg)
+        logger.info(msg)
 
     def __show_bot_stopped(self):
         self.send_message(ADMIN_ID, BOT_STOP)
@@ -308,22 +308,22 @@ class TelegramBot(telebot.TeleBot):
         elif alert.type == common.T_CAM_MOVE_PHOTO:
             logger.info("Alert: {:s} saved photo in {:s}".format(alert.msg,
                                                                  alert.img))
-            self.send_message(chat_id=ADMIN_ID, text="MOVE photo:\n{:s}".format(alert.msg))
+            # self.send_message(chat_id=ADMIN_ID, text="MOVE photo:\n{:s}".format(alert.msg))
             self.send_photo(chat_id=ADMIN_ID, photo=open(alert.img, 'rb'))
 
             for i in range(0, self.__model.get_viewers_len()):
                 (s_id, s_name) = self.__model.get_viewer_by_i(i)
-                self.send_message(chat_id=s_id, text="MOVE photo:\n{:s}".format(alert.msg))
+                # self.send_message(chat_id=s_id, text="MOVE photo:\n{:s}".format(alert.msg))
                 self.send_photo(chat_id=s_id, photo=open(alert.img, 'rb'))
         elif alert.type == common.T_CAM_MOVE_MP4:
             logger.info("Alert: {:s} saved video in {:s}".format(alert.msg,
                                                                  alert.img))
-            self.send_message(chat_id=ADMIN_ID, text="MOVE video:\n{:s}".format(alert.msg))
+            # self.send_message(chat_id=ADMIN_ID, text="MOVE video:\n{:s}".format(alert.msg))
             self.send_video(chat_id=ADMIN_ID, data=open(alert.img, 'rb'))
 
             for i in range(0, self.__model.get_viewers_len()):
                 (s_id, s_name) = self.__model.get_viewer_by_i(i)
-                self.send_message(chat_id=s_id, text="MOVE video:\n{:s}".format(alert.msg))
+                # self.send_message(chat_id=s_id, text="MOVE video:\n{:s}".format(alert.msg))
                 self.send_video(chat_id=s_id, data=open(alert.img, 'rb'))
         elif alert.type in (common.T_SYS_NOW_INFO, common.T_SYS_ALERT):
             self.send_message(chat_id=ADMIN_ID, text=alert.msg, parse_mode="markdown")
